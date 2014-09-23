136d72668ad453ec3516514735c0daf245abb125
Norbert Kozłowski <norbert.kozlowski@intenso.com.pl>
2014-09-23 08:51:04 +0200

    Synchronizing static method

--- Stemmer/src/com/khozzy/stemmer/stemming/PolishStemming.java
+++ Stemmer/src/com/khozzy/stemmer/stemming/PolishStemming.java
@@ -4,36 +4,41 @@ import morfologik.stemming.Dictionary;
 import morfologik.stemming.DictionaryLookup;
 import morfologik.stemming.WordData;
 
+import java.lang.Object;
 import java.util.List;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public class PolishStemming {
 
-    final static Pattern UNDESIRABLES = Pattern.compile("[,.;!?(){}\\[\\]<>%]");
+    private final static Object LOCK = new Object() {};
+    private final static Pattern UNDESIRABLES = Pattern.compile("[,.;!?(){}\\[\\]<>%]");
 
-    final static Dictionary polish = Dictionary.getForLanguage("pl");
-    final static DictionaryLookup dl = new DictionaryLookup(polish);
+    private final static Dictionary polish = Dictionary.getForLanguage("pl");
+    private final static DictionaryLookup dl = new DictionaryLookup(polish);
 
-    public static synchronized String process(final String sentence) {
-        StringBuilder processed = new StringBuilder();
-        final String[] words = sentence
-                .toLowerCase()
-                .split(" ");
+    public static String process(final String sentence) {
+        synchronized (LOCK) {
+            StringBuilder processed = new StringBuilder();
 
-        Stream
-                .of(words)
-                .forEach(w -> {
-                    w = UNDESIRABLES.matcher(w).replaceAll("");
+            final String[] words = sentence
+                    .toLowerCase()
+                    .split(" ");
 
-                    List<WordData> wordList = dl.lookup(w);
+            Stream
+                    .of(words)
+                    .forEach(w -> {
+                        w = UNDESIRABLES.matcher(w).replaceAll("");
 
-                    if (!wordList.isEmpty()) {
-                        processed.append(wordList.get(0).getStem());
-                        processed.append(" ");
-                    }
-                });
+                        List<WordData> wordList = dl.lookup(w);
 
-        return processed.toString().trim();
+                        if (!wordList.isEmpty()) {
+                            processed.append(wordList.get(0).getStem());
+                            processed.append(" ");
+                        }
+                    });
+
+            return processed.toString().trim();
+        }
     }
 }
